72ac2db67df5509715100a3ba24b9f223ec0dfc1
tj-cool <79023922+tj-cool@users.noreply.github.com>
2023-05-20 12:13:00 -0500

    Original commit

---
+++ tjgames.js
@@ -0,0 +1,15 @@
+// Wait for the DOM content to load
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the button element by its ID
+    var fbButton = document.getElementById('fb');
+  
+    // Add an event listener to the button
+    fbButton.onclick = function() {
+        window.location = "fb.html"
+    }
+  });
+  
+  function startFlappyBirdGame() {
+    // Add your code here to start the Flappy Bird game
+    // This function will be called when the Flappy Bird button is clicked
+  }
